e06475cf428b91013b79de57b924bc55c044d4b5
seokyung402 <wni402@naver.com>
2024-04-16 16:09:37 +0900

    실습

---
+++ OXQuiz/OXQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  OXQuizApp.swift
+//  OXQuiz
+//
+//  Created by 이서경 on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct OXQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
